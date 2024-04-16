0fa85fa16095de122ac8f659c68c20f8dbfa6b7a
Nikos Dimitrakopoulos <nikossoftwaredev@gmail.com>
2024-04-16 16:27:19 +0300

    aaaa

--- validator.sh
+++ validator.sh
@@ -8,13 +8,21 @@ exec solana-validator \
     --ledger /var/nikossoftwaredev/data/ledger \
     --trusted-validator 7Np41oeYqPefeNQEHSv1UDhYrehxin3NStELsSKCT4K2 \
     --trusted-validator GdnSyH3YtwcxFvQrVVJMm1JhTS4QVX7MFsX56uJLUfiZ \
+    --trusted-validator DE1bawNcRJB9rVm3buyMVfr8mBEoyyu73NBovf2oXJsJ \
+    --trusted-validator CakcnaRDHka2gXyfbEd2d3xsvkJkqsLw2akB3zsN1D2S \
     --expected-genesis-hash 5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d \
     --entrypoint entrypoint.mainnet-beta.solana.com:8001 \
+    --entrypoint entrypoint2.mainnet-beta.solana.com:8001 \
+    --entrypoint entrypoint3.mainnet-beta.solana.com:8001 \
+    --entrypoint entrypoint4.mainnet-beta.solana.com:8001 \
+    --entrypoint entrypoint5.mainnet-beta.solana.com:8001 \
     --no-voting \
-    --snapshot-interval-slots 1000 \
+    --snapshot-interval-slots 500 \
+    --maximum-local-snapshot-age 500 \
     --rpc-port 8899 \
     --rpc-bind-address 0.0.0.0 \
-    --dynamic-port-range 8000-8014 \
+    --dynamic-port-range 8000-8020 \
+    --no-port-check \
     --wal-recovery-mode skip_any_corrupted_record \
     --init-complete-file /var/nikossoftwaredev/data/init-completed \
     --snapshot-archive-format lz4 \
