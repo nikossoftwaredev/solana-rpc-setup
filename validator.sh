bde68446eed84a288d43a14a16ac3c016da5cc74
Nikos Dimitrakopoulos <nikossoftwaredev@gmail.com>
2024-04-16 15:05:37 +0300

    added validator

--- validator.sh
+++ validator.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 exec solana-validator \
     --identity /home/root/solana-rpc-setup/solana-keys/validator-keypair.json \
-    --known-validator 5D1fNXzvv5NjV1ysLjirC4WY92RNsVH18vjmcszZd8on \
-    --known-validator dDzy5SR3AXdYWVqbDEkVFdvSPCtS9ihF5kJkHCtXoFs \
-    --known-validator eoKpUABi59aT4rR9HGS3LcMecfut9x7zJyodWWP43YQ \
-    --known-validator 7XSY3MrYnK8vq693Rju17bbPkCN3Z7KvvfvJx4kdrsSY \
-    --known-validator Ft5fbkqNa76vnsjYNwjDZUXoTWpP7VYm3mtsaQckQADN \
-    --known-validator 9QxCLckBiJc783jnMvXZubK4wH86Eqqvashtrwvcsgkv \
-    --only-known-rpc \
-    --full-rpc-api \
+    --ledger /var/solana/data/ledger \
+    --trusted-validator 7Np41oeYqPefeNQEHSv1UDhYrehxin3NStELsSKCT4K2 \
+    --trusted-validator GdnSyH3YtwcxFvQrVVJMm1JhTS4QVX7MFsX56uJLUfiZ \
+    --trusted-validator DE1bawNcRJB9rVm3buyMVfr8mBEoyyu73NBovf2oXJsJ \
+    --trusted-validator CakcnaRDHka2gXyfbEd2d3xsvkJkqsLw2akB3zsN1D2S \
+    --expected-genesis-hash 5eykt4UsFv8P8NJdTREpY1vzqKqZKvdpKuc147dw2N9d \
+    --entrypoint entrypoint.mainnet-beta.solana.com:8001 \
+    --entrypoint entrypoint2.mainnet-beta.solana.com:8001 \
+    --entrypoint entrypoint3.mainnet-beta.solana.com:8001 \
+    --entrypoint entrypoint4.mainnet-beta.solana.com:8001 \
+    --entrypoint entrypoint5.mainnet-beta.solana.com:8001 \
     --no-voting \
-    --ledger /mnt/ledger \
-    --accounts /mnt/accounts \
-    --log /home/sol/solana-rpc.log \
+    --snapshot-interval-slots 500 \
+    --maximum-local-snapshot-age 500 \
     --rpc-port 8899 \
     --rpc-bind-address 0.0.0.0 \
-    --private-rpc \
     --dynamic-port-range 8000-8020 \
-    --entrypoint entrypoint.testnet.solana.com:8001 \
-    --entrypoint entrypoint2.testnet.solana.com:8001 \
-    --entrypoint entrypoint3.testnet.solana.com:8001 \
-    --expected-genesis-hash 4uhcVJyU9pJkvQyS88uRDiswHXSCkY3zQawwpjk2NsNY \
+
+    --no-port-check \
     --wal-recovery-mode skip_any_corrupted_record \
-    --limit-ledger-size
+    --enable-rpc-transaction-history \
+    --enable-cpi-and-log-storage \
+    --init-complete-file /var/solana/data/init-completed \
+    --snapshot-compression none \
+    --require-tower \
+    --no-wait-for-vote-to-start-leader \
+    --no-poh-speed-test \
+    --limit-ledger-size 50000000 \
+    --accounts /var/solana/accounts \
+    --log 
