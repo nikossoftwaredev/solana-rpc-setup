64c9b5d439a16cc879d4f2c4341989dc14c04a36
Nikos Dimitrakopoulos <nikossoftwaredev@gmail.com>
2024-04-16 15:14:44 +0300

    423

--- main-setup.sh
+++ main-setup.sh
@@ -10,16 +10,13 @@ echo "Creating Solana user..."
 sudo useradd -m -s /bin/bash solana
 sudo usermod -aG sudo solana
 
-# Switch to the solana user
-echo "Switching to solana user..."
-sudo -u solana bash << EOF
-
 echo "Installing Solana..."
 sh -c "$(curl -sSfL https://release.solana.com/stable/install)"
 
 # Now, immediately export and use the new PATH
-echo 'export PATH="$PATH:/root/.local/share/solana/install/active_release/bin"' >> ~/.bashrc
+echo 'export PATH="$PATH:solana/.local/share/solana/install/active_release/bin"' >> ~/.bashrc
 source ~/.bashrc
+
 
 echo "Step 1: System Tuning..."
 ./1-system-tuning.sh
@@ -29,5 +26,3 @@ echo "Step 2: Starting Rpc..."
 ./2-start-rpc.sh
 
 echo "Setup complete! Solana Validator should be operational."
-
-EOF
